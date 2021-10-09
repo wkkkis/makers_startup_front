dcb631447012fab1808d9a1531969c840e0451d3
Issak <aizekyuy@yandex.ru>
2021-10-09 19:17:47 +0600

    added react-icons library

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 //Styles
@@ -6,18 +5,11 @@ import "./styles/app.scss";
 
 //Routes
 import AppRouter from './routes/AppRouter';
-=======
-import AppRouter from "./routes/AppRouter";
->>>>>>> 57350c01ed2de23a0a7c1af3ece6ee14d3d1328a
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <AppRouter />
-=======
-    <AppRouter />
->>>>>>> 57350c01ed2de23a0a7c1af3ece6ee14d3d1328a
     </>
   );
 }
